203ab3afb6bcad8da28e7d67195d14b4e3fe6a26
joseangelgonzalez <joseangelgonzalez@DESKTOP-H7KDU47>
2019-06-03 20:35:14 -0500

    mejoras version 1.0

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-      echo 'BIENVENIDOS-----gggggg--+++++++++'
+      echo 'BIENVENIDOS--+++++---ggggggllll--+++++++++'
         ?>
     </body>
 </html>
